9490ebf9f569aea5cd40e5b4baafcddec0a8df0c
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-10-02 15:39:17 +0300

    handlebars and rendering overview

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const path = require('path');
+const handlebars = require('express-handlebars');
+const controller = require('./controllers');
+
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('hbs', handlebars({
+  extname: 'hbs',
+  layoutsDir: path.join(__dirname, 'views', 'layouts'),
+  partialsDir: path.join(__dirname, 'views', 'partials'),
+  defaultLayout: 'main',
+}));
+console.log(55);
+
 app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(controller);
 
 module.exports = app;
