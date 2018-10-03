34ddd410b65a5bb04714239170bc1fa5a65573d9
hemasali <hemo3li7@gmail.com>
2018-10-03 09:23:36 +0300

    d

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 const models = require('./database/dbConnect');
 
+
 models.connection.sync({ force: true }).then(() => {
   app.listen(app.get('port'), () => {
     console.log('App running on port', app.get('port'));
