f40e948b6823b94882d93a64c692e1b182652558
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-10-03 15:16:16 +0300

    update data from database

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const overview = require('./overview');
 const generalSettings = require('./generalSettings');
+const convertCamelToSnake = require('../middleware/convertCamelToSnake');
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ router.get('/', overview.get);
 
 // Setting Page //
 router.get('/generalSetting', generalSettings.get);
+router.post('/generalSetting', convertCamelToSnake, generalSettings.post);
 
 module.exports = router;
