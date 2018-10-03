e9216b22641de39eada89170afecf2d2a744c2a0
hemasali <hemo3li7@gmail.com>
2018-10-03 11:54:27 +0300

    get list of employees from database relates to #25

--- src/controllers/employees.js
+++ src/controllers/employees.js
@@ -1,3 +1,9 @@
+const employees = require('../database/models/employee');
+
 exports.get = (request, response) => {
-  response.render('employees', { cssFile: 'tables' });
+  employees.findAll({ attributes: ['name', ['id_number', 'idNumber'], ['job_title', 'jobTitle'], 'class'] }).then((result) => {
+    console.log(result);
+    
+    response.render('employees', { result, cssFile: 'tables' });
+  });
 };
