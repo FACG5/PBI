c883edff86010d02abd67353ba5de2e1d52572f8
othman-shamla <othmansh2010@gmail.com>
2018-10-04 05:29:10 +0300

    create update route for employee

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,6 +4,7 @@ const employees = require('./employees');
 const addEmployee = require('./addEmployee');
 const setting = require('./setting');
 const convertToSnake = require('../middleware/convertCamelToSnake');
+const editEmployee = require('./editEmployee');
 
 const router = express.Router();
 
@@ -11,6 +12,7 @@ router.get('/', overview.get);
 router.get('/employees', employees.get);
 router.get('/addEmployee', addEmployee.get);
 router.post('/addEmployee', convertToSnake, addEmployee.post);
+router.put('/employee', convertToSnake, editEmployee.put);
 router.get('/setting', setting.get);
 
 module.exports = router;
