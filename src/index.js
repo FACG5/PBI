4611411584f364b57673e1a21942ddf1353722b2
hemasali <hemo3li7@gmail.com>
2018-10-04 17:20:50 +0300

    fixing colum in database

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const { sequelize } = require('./database/models');
 
-sequelize.sync({force:true}).then(() => {
+sequelize.sync().then(() => {
   app.listen(app.get('port'), () => {
     console.log('App running on port', app.get('port'));
   });
