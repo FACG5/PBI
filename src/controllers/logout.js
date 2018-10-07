7b43b0a01666ab677abe77160a8a735af8159a35
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-10-07 11:13:14 +0300

    solve requested changes

--- src/controllers/logout.js
+++ src/controllers/logout.js
@@ -1,4 +1,4 @@
 exports.get = (req, res) => {
   res.clearCookie('jwt');
-  res.redirect('/login');
+  res.send(JSON.stringify({ err: null, message: 'تم تسجيل الخروج , سيتم تحويلك إلى صفحة تسجيل الدخول' }));
 };
