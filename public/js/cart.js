865de5c085523bf6742db700e17480ae7f5f180d
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-10-08 11:01:39 +0300

    make the fetch for update data

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,10 +1,12 @@
 const trash = document.querySelectorAll('#trash');
 const add = document.getElementById('add');
 const inputField = document.querySelector('#inputText');
+const edit = document.querySelectorAll('#edit');
 
 trash.forEach((element) => {
-  element.addEventListener('click', () => {
-    const boxId = element.getAttribute('boxId');
+  element.addEventListener('click', (event) => {
+    const parentRow = event.target.parentElement.parentElement;
+    const boxId = parentRow.getAttribute('boxId');
     swal({
       title: '',
       text: 'بمجرد حذف الصندوق ﻻ يمكنك الاسترجاع',
@@ -57,4 +59,46 @@ add.addEventListener('click', () => {
         });
       }
     });
+});
+
+edit.forEach((element) => {
+  element.addEventListener('click', (event) => {
+    const parentRow = event.target.parentElement.parentElement;
+    const cartName = parentRow.querySelector('#cartName');
+    cartName.readOnly = false;
+    cartName.classList.add('inputEditable');
+    element.style.display = 'none';
+    const parentIcons = event.target.parentElement;
+    const check = document.createElement('i');
+    check.classList.add('fas', 'fa-check', 'check');
+    parentIcons.appendChild(check);
+    check.addEventListener('click', () => {
+      cartName.classList.remove('inputEditable');
+      element.style.display = 'inline-block';
+      check.style.display = 'none';
+      const boxId = parentRow.getAttribute('boxId');
+      const newData = {
+        name: cartName.value,
+        boxId,
+      };
+      fetch('/carts', {
+        method: 'PUT',
+        body: JSON.stringify(newData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then(result => result.json())
+        .then((result) => {
+          if (result.err) {
+            swal('', result.err, 'error').then(() => {
+              window.location.href = '/carts';
+            });
+          } else {
+            swal('', result.message, 'success').then(() => {
+              window.location.href = '/carts';
+            });
+          }
+        });
+    });
+  });
 });
