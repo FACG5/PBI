adff13d60c18e6a3457992a9636481e49098fcb0
hemasali <hemo3li7@gmail.com>
2018-10-08 15:58:37 +0300

    function which upload files

---
+++ src/controllers/files.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+exports.post = (request, response, next) => {
+  const { cerfications } = request.files;
+  const cerficationsName = cerfications.name;
+  const splitCerfications = cerficationsName.split('.');
+  const nameOfFile = splitCerfications[0];
+  const ext = splitCerfications[1];
+  const renderStream = request.files.cerfications.data;
+  const path = `uploads/${nameOfFile}.${ext}`;
+
+  fs.writeFile(path, renderStream, (err) => {
+    if (err) {
+      next(err);
+    }
+    response.send(JSON.stringify({ path }));
+  });
+};
