40802a01685ddb255850e0227acc8d14ce6a97ee
hemasali <hemo3li7@gmail.com>
2018-10-09 16:14:24 +0300

    function of downloadExcel in report controller

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -1,4 +1,6 @@
 const salaryMain = require('./helpers/salaryMain');
+const excelMain = require('./helpers/excelSheets/excelSheetMain');
+
 const { report } = require('../database/models');
 
 exports.get = (req, res) => {
@@ -9,4 +11,10 @@ exports.post = async (req, res) => {
   const { dateValue } = req.body;
   const reports = await salaryMain(dateValue);
   reports.map(reportElement => report.upsert(reportElement));
+};
+
+exports.downloadExcel = async (req, res) => {
+  const { date } = req.params;
+  const reports = await excelMain(date);
+  res.send(reports);
 };
