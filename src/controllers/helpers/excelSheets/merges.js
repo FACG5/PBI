bf8dd933c7df58f9bab17c5fd25a94695d564cb5
hemasali <hemo3li7@gmail.com>
2018-10-09 16:01:49 +0300

    defining the merges of cells in excel sheet

---
+++ src/controllers/helpers/excelSheets/merges.js
@@ -0,0 +1,10 @@
+
+const merges = [
+  { start: { row: 3, column: 3 }, end: { row: 4, column: 15 } },
+
+];
+for (let i = 1; i < 38; i++) {
+  const merge = { start: { row: 6, column: i }, end: { row: 7, column: i } };
+  merges.push(merge);
+}
+module.exports = merges;
