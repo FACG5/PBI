fcfb57172c58c6acf1125bdf9c575e4465dba068
hemasali <hemo3li7@gmail.com>
2018-10-09 16:02:46 +0300

    define the heading of excel sheet

---
+++ src/controllers/helpers/excelSheets/heading.js
@@ -0,0 +1,18 @@
+const styles = require('./styles');
+
+const heading = [
+  [],
+  [],
+  [
+    '',
+    '',
+    {
+      value: 'رواتب موظفي المعهد عن شهر أيلول 2018',
+      style: styles.headerDark,
+    },
+  ],
+  [],
+  [],
+];
+
+module.exports = heading;
