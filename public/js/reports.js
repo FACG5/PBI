1a3b486939b6635fe9d1c37b9d47358cdb85bae5
hemasali <hemo3li7@gmail.com>
2018-10-09 16:09:25 +0300

    adding event listener to the button of export report

--- public/js/reports.js
+++ public/js/reports.js
@@ -1,5 +1,7 @@
 const createButton = document.getElementById('create');
 const date = document.getElementById('month');
+const saveButton = document.getElementById('save');
+
 createButton.addEventListener('click', () => {
   const dateValue = date.value;
   const dateObject = { dateValue };
@@ -10,4 +12,24 @@ createButton.addEventListener('click', () => {
       'Content-Type': 'application/json',
     },
   });
+});
+
+saveButton.addEventListener('click', () => {
+  const dateValue = date.value;
+  fetch(`/downloadReports/${dateValue}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(result => result.blob())
+    .then((report) => {
+      const url = window.URL.createObjectURL(report);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = `PBI - Report of ${dateValue}.xlsx`;
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+    });
 });
