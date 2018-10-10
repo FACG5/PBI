4e97f212638fbd3eaff19483bc22865b85df928c
hemasali <hemo3li7@gmail.com>
2018-10-10 15:38:41 +0300

    fixing conflicts

--- src/database/query/deductionsQuery.js
+++ src/database/query/deductionsQuery.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/connection');
 const deductionsQuery = async (employee) => {
   const { employeeId } = employee;
   const result = await sequelize.query(
-    'select deductions_health_insurance,deductions_loans,deductions_social_fund,(salary*(select saving_ratio from fixed_varibles where id = 1)) AS saving ,(select coalesce (SUM(payment),0) AS purchase_boxes from purchases_employees where employee_id = :employeeId )from employees where id = :employeeId', { replacements: { employeeId } },
+    'select deductions_health_insurance,deductions_loans,deductions_social_fund,(salary*(select saving_ratio from fixed_varibles where id = 1)) AS savings ,(select coalesce (SUM(payment),0) AS purchase_boxes from purchases_employees where employee_id = :employeeId )from employees where id = :employeeId', { replacements: { employeeId } },
   );
   return result[0][0];
 };
--- src/database/query/exemptions.js
+++ src/database/query/exemptions.js
@@ -1,13 +1,15 @@
 const sequelize = require('../config/connection');
 
-const exemptions = async (employee, variables) => {
+const exemptions = async (employee) => {
   const { totalAllownace, employeeId } = employee;
-  const { dolar } = variables;
-
-  const result = await sequelize.query(`select exemption_university , exemption_house ,exemption_resident , ( select CASE when transportation > 0.1*(salary+${totalAllownace}) then (0.1*(salary+${totalAllownace})*${dolar}) else (transportation * ${dolar}) END AS "transportationsExemptions")`
-+ `from employees where id=${employeeId}`);
-  const employeeExemptions = result[0][0];
-  return employeeExemptions;
+  const result = await sequelize.query(
+    `select exemption_university , exemption_house ,exemption_resident ,
+     ( select CASE when transportation > 0.1*(salary+${totalAllownace}) 
+     then (0.1*(salary+${totalAllownace})*(select saving_ratio from fixed_varibles where id = 1)) 
+     else (transportation * (select dolar from fixed_varibles where id = 1)) END AS
+      "transportationsExemptions")from employees where id=${employeeId}`,
+  );
+  return result[0][0];
 };
 
 module.exports = exemptions;
