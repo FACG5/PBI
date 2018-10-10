d3047be317920fab7909f17d4b426f341632c7a1
hemasali <hemo3li7@gmail.com>
2018-10-10 13:20:36 +0300

    handle errors

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -3,17 +3,33 @@ const { report } = require('../database/models');
 
 exports.get = (req, res) => {
   res.render('reports', {
-    cssFile: ['reports'], jsFile: ['reports'], activePage: { reports: true }, title: 'تقارير الموظفين',
+    cssFile: ['reports'],
+    jsFile: ['reports'],
+    activePage: { reports: true },
+    title: 'تقارير الموظفين',
   });
 };
 
-exports.post = async (req, res) => {
+exports.post = async (req, res, next) => {
   const { dateValue } = req.body;
-  const reports = await salaryMain(dateValue);
-  reports.map((reportElement) => {
-    report.findAll({ where: { id_number: reportElement.id_number, date: dateValue } }).then((result) => {
-      if (result.length) return report.update(reportElement, { where: { id_number: reportElement.id_number, date: dateValue } });
-      return report.create(reportElement);
+  try {
+    const reports = await salaryMain(dateValue);
+    reports.map((reportElement) => {
+      report
+        .findAll({
+          where: { id_number: reportElement.id_number, date: dateValue },
+        })
+        .then((result) => {
+          if (result.length) {
+            return report.update(reportElement, {
+              where: { id_number: reportElement.id_number, date: dateValue },
+            });
+          }
+          return report.create(reportElement);
+        });
     });
-  });
+    res.send({ err: null, message: '' });
+  } catch (err) {
+    next(err);
+  }
 };
