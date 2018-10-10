724822f3fc0be2290e9a37c032af70509fad0407
hemasali <hemo3li7@gmail.com>
2018-10-10 22:47:12 +0300

    use async/await insted of Promis

--- src/controllers/helpers/excelSheets/excelSheetMain.js
+++ src/controllers/helpers/excelSheets/excelSheetMain.js
@@ -5,21 +5,23 @@ const specification = require('./columns');
 const getReports = require('./dataSet');
 const merges = require('./merges');
 
-const excelMain = date => new Promise((resolve, reject) => {
-  getReports(date).then((reportResult) => {
-    let data = [];
-    data = reportResult.map(convertToCamelCase);
-    const heading = head(date);
-    const reportSheet = excel.buildExport([
-      {
-        name: 'Report',
-        heading,
-        merges,
-        specification,
-        data,
-      },
-    ]);
-    resolve(reportSheet);
-  });
-});
+
+const excelMain = async (date) => {
+  const reportResult = await getReports(date);
+  let data = [];
+  data = reportResult.map(convertToCamelCase);
+  const heading = head(date);
+  const reportSheet = excel.buildExport([
+    {
+      name: 'Report',
+      heading,
+      merges,
+      specification,
+      data,
+    },
+  ]);
+  return reportSheet;
+};
+
+
 module.exports = excelMain;
