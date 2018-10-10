66a423bae9a47eed1c89536c697f8ec2efc54170
hemasali <hemo3li7@gmail.com>
2018-10-10 13:20:07 +0300

    using async/await whit queries

--- src/database/query/deductionsQuery.js
+++ src/database/query/deductionsQuery.js
@@ -1,18 +1,14 @@
 const sequelize = require('../config/connection');
 
-const deductionsQuery = (employee, variables) => new Promise((resolve, reject) => {
+const deductionsQuery = async (employee, variables) => {
   const { savingRatio } = variables;
   const { employeeId } = employee;
-  sequelize
-    .query(
-      `select deductions_health_insurance,deductions_loans,deductions_social_fund,(salary*${savingRatio}) AS saving ,(select coalesce (SUM(payment),0) AS purchase_boxes from purchases_employees where employee_id = ${employeeId})`
-          + `from employees where id = ${employeeId}`,
-    )
-    .then((result) => {
-      const employeeDeductions = result[0][0];
-      resolve(employeeDeductions);
-    })
-    .catch(err => reject(err));
-});
+  const result = await sequelize.query(
+    `select deductions_health_insurance,deductions_loans,deductions_social_fund,(salary*${savingRatio}) AS saving ,(select coalesce (SUM(payment),0) AS purchase_boxes from purchases_employees where employee_id = ${employeeId})`
+      + `from employees where id = ${employeeId}`,
+  );
+  const employeeDeductions = result[0][0];
+  return employeeDeductions;
+};
 
 module.exports = deductionsQuery;
