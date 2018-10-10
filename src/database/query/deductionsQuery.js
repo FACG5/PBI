96ae56383c7beed35cd9bbe26c3e2295c7f52c70
othman-shamla <othmansh2010@gmail.com>
2018-10-10 12:04:49 +0300

    replace promise with async await

--- src/database/query/deductionsQuery.js
+++ src/database/query/deductionsQuery.js
@@ -1,18 +1,11 @@
 const sequelize = require('../config/connection');
 
-const deductionsQuery = (employee, variables) => new Promise((resolve, reject) => {
-  const { savingRatio } = variables;
+const deductionsQuery = async (employee) => {
   const { employeeId } = employee;
-  sequelize
-    .query(
-      `select deductions_health_insurance,deductions_loans,deductions_social_fund,(salary*${savingRatio}) AS saving ,(select coalesce (SUM(payment),0) AS purchase_boxes from purchases_employees where employee_id = ${employeeId})`
-          + `from employees where id = ${employeeId}`,
-    )
-    .then((result) => {
-      const employeeDeductions = result[0][0];
-      resolve(employeeDeductions);
-    })
-    .catch(err => reject(err));
-});
+  const result = await sequelize.query(
+    'select deductions_health_insurance,deductions_loans,deductions_social_fund,(salary*(select saving_ratio from fixed_varibles where id = 1)) AS saving ,(select coalesce (SUM(payment),0) AS purchase_boxes from purchases_employees where employee_id = :employeeId )from employees where id = :employeeId', { replacements: { employeeId } },
+  );
+  return result[0][0];
+};
 
 module.exports = deductionsQuery;
--- src/database/query/exemptions.js
+++ src/database/query/exemptions.js
@@ -1,14 +1,9 @@
 const sequelize = require('../config/connection');
 
-const exemptions = (employee, variables) => new Promise((resolve, reject) => {
+const exemptions = async (employee) => {
   const { totalAllownace, employeeId } = employee;
-  const { dolar } = variables;
+  const result = await sequelize.query(`select exemption_university , exemption_house ,exemption_resident , ( select CASE when transportation > 0.1*(salary+${totalAllownace}) then (0.1*(salary+${totalAllownace})*(select saving_ratio from fixed_varibles where id = 1)) else (transportation * (select dolar from fixed_varibles where id = 1)) END AS "transportationsExemptions")from employees where id=${employeeId}`);
+  return result[0][0];
+};
 
-  sequelize.query(`select exemption_university , exemption_house ,exemption_resident , ( select CASE when transportation > 0.1*(salary+${totalAllownace}) then (0.1*(salary+${totalAllownace})*${dolar}) else (transportation * ${dolar}) END AS "transportationsExemptions")`
-+ `from employees where id=${employeeId}`)
-    .then((result) => {
-      const employeeExemptions = result[0][0];
-      resolve(employeeExemptions);
-    }).catch(err => reject(err));
-});
 module.exports = exemptions;
