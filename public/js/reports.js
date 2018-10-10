98d18524e1fddbb85218ea000557732185940cc2
hemasali <hemo3li7@gmail.com>
2018-10-10 13:17:42 +0300

    confirm box when choosing month

--- public/js/reports.js
+++ public/js/reports.js
@@ -3,11 +3,24 @@ const date = document.getElementById('month');
 createButton.addEventListener('click', () => {
   const dateValue = date.value;
   const dateObject = { dateValue };
-  fetch('/reports', {
-    method: 'POST',
-    body: JSON.stringify(dateObject),
-    headers: {
-      'Content-Type': 'application/json',
-    },
+  swal({
+    title: `سيتم إنشاء تقارير لشهر ${dateValue} `,
+    text: 'سيتم الكتابة فوق تقارير الشهر الذي تم اختياره',
+    type: 'warning',
+    showCancelButton: true,
+  }).then((confirm) => {
+    if (confirm.value) {
+      fetch('/reports', {
+        method: 'POST',
+        body: JSON.stringify(dateObject),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then(result => result.json())
+        .then((result) => {
+          if (result.err) return swal('', result.err, 'error');
+          return swal('', 'تم إنشاء التقارير المطلوبة , يمكنك تحميلها', 'success');
+        });
+    }
   });
 });
