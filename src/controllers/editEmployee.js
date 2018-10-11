2dc123a5e241eebebee956092d0c9414281424aa
hemasali <hemo3li7@gmail.com>
2018-10-11 19:45:07 +0300

    get new payment obecjt with value and id of each purchase box , which sent from front end

--- src/controllers/editEmployee.js
+++ src/controllers/editEmployee.js
@@ -1,8 +1,13 @@
 const { employee } = require('./../database/models');
+const updatePurchase = require('../database/query/updatePurchase');
 
 exports.put = async (req, res, next) => {
   try {
-    const targetEmployee = await employee.findById(req.body.id);
+    const { payment, id } = req.body;
+    if (payment) {
+      updatePurchase(payment, id);
+    }
+    const targetEmployee = await employee.findById(id);
     await targetEmployee.updateAttributes(req.body);
     res.send({ err: null, message: 'تم تحديث معلومات الموظف بنجاح' });
   } catch (err) {
